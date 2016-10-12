b181430286e937379573f13a497c7335f7256a8e
a.walker <a.walker@adito.de>
2016-10-12 11:36:36 +0200

    add 2.4 version

--- icinga2.sh
+++ icinga2.sh
@@ -10,12 +10,12 @@ ICINGA_PASS="icinga"
 #Icinga-trusty i386 amd64
 apt-get update
 apt-get install python-software-properties software-properties-common apache2 vim heirloom-mailx supervisor -y
-add-apt-repository ppa:formorer/icinga -y
+#add-apt-repository ppa:formorer/icinga -y
 
 # Patchen des Systems
 
-apt-get update
-apt-get upgrade -y
+#apt-get update
+#apt-get upgrade -y
 
 # Installation icinga2
 apt-get install icinga2 -y
